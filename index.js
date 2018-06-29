5b005ba6cc4864c91794b273682baee1f2ea52d1
Jacky Tang <jackytck@gmail.com>
2018-06-29 23:05:36 +0800

    :construction: use express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hi there')
+})
+
+app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))

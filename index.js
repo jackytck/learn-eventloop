e52601927e3c958706252512ae205e48c20eddea
Jacky Tang <jackytck@gmail.com>
2018-06-30 22:02:54 +0800

    :construction: test using cluster mode

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cluster = require('cluster')
 const express = require('express')
 const app = express()
 
@@ -7,9 +8,25 @@ function doWork (duration) {
   }
 }
 
-app.get('/', (req, res) => {
-  doWork(5000)
-  res.send('Hi there')
-})
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but
+  // in child mode
+  cluster.fork()
+  // cluster.fork()
+  // cluster.fork()
+  // cluster.fork()
+} else {
+  // I'm a child, I'm going to act like a server
+  // and do nothing else
+  app.get('/', (req, res) => {
+    doWork(5000)
+    res.send('Hi there')
+  })
 
-app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!')
+  })
+
+  app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))
+}

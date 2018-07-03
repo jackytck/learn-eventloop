02f057680273c00a822e4347a9aa3ee5b03d9521
Jacky Tang <jackytck@gmail.com>
2018-07-03 22:16:20 +0800

    :fire: clean up cluster code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 process.env.UV_THREADPOOL_SIZE = 1 // every child in the cluster would have 1 thread
-const cluster = require('cluster')
 const crypto = require('crypto')
 const express = require('express')
 const app = express()
@@ -10,22 +9,14 @@ function doWork (res) {
   })
 }
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but
-  // in child mode
-  cluster.fork()
-  cluster.fork()
-} else {
-  // I'm a child, I'm going to act like a server
-  // and do nothing else
-  app.get('/', (req, res) => {
-    doWork(res)
-  })
+// I'm a child, I'm going to act like a server
+// and do nothing else
+app.get('/', (req, res) => {
+  doWork(res)
+})
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast!')
-  })
+app.get('/fast', (req, res) => {
+  res.send('This was fast!')
+})
 
-  app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))
-}
+app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))

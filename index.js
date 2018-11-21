0241fab17f3316372cac1407427dbb0c83d0f9f3
Jacky Tang <jackytck@gmail.com>
2018-11-21 22:56:03 +0800

    :hammer: webworker thread demo

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 process.env.UV_THREADPOOL_SIZE = 1 // every child in the cluster would have 1 thread
-const crypto = require('crypto')
+// const crypto = require('crypto')
 const express = require('express')
 const app = express()
+const Worker = require('webworker-threads').Worker
 
 function doWork (res) {
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there')
+  const worker = new Worker(function () {
+    // inside worker, handle server's postMessage
+    this.onmessage = function () {
+      let counter = 0
+      while (counter < 1e9) {
+        counter++
+      }
+      // worker to server
+      postMessage('' + counter)
+    }
   })
+
+  // server side, handle from worker's postMessage
+  worker.onmessage = function (message) {
+    console.log(message.data)
+    res.send(message.data)
+  }
+
+  // server to worker
+  worker.postMessage()
 }
 
 // I'm a child, I'm going to act like a server
@@ -19,4 +37,4 @@ app.get('/fast', (req, res) => {
   res.send('This was fast!')
 })
 
-app.listen(3000, () => console.log('Listening at 127.0.0.1:3000...'))
+app.listen(3000, () => console.log('Listening at http://127.0.0.1:3000...'))
